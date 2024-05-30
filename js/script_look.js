3c84fbddd6183983640bbff4ddcff5b4c2d75641
JeonSeokMin <wjstjrals417@gmail.com>
2024-05-30 23:33:35 +0900

    test commit

---
+++ js/script_look.js
@@ -0,0 +1,8 @@
+const urlStr = window.location.href;
+
+console.log(urlStr)
+
+
+// boards/id -> id 값 가져와야함 가져와서 다시 get 요청
+
+
--- js/script_make.js
+++ js/script_make.js
@@ -10,7 +10,6 @@ document.getElementById('postForm').addEventListener('submit', function(event) {
       content: content,
       productName: productName
     };
-  
     axios.post('https://3.36.250.80:3000/api/boards', postData, {
       headers: {
         'Content-Type': 'application/json'
@@ -18,11 +17,14 @@ document.getElementById('postForm').addEventListener('submit', function(event) {
     })
     .then(response => {
       console.log('Success:', response.data);
-      // 성공 시 사용자가 볼 수 있도록 메시지를 표시하거나 다른 동작을 수행합니다.
+      alert('게시물이 성공적으로 업로드되었습니다.');
+      // 성공 시 사용자가 볼 수 있도록 메시지를 표시한 후 페이지를 이동합니다.
+      window.location.href = 'look_post.html';
     })
     .catch(error => {
       console.error('Error:', error);
-      // 실패 시 사용자가 볼 수 있도록 에러 메시지를 표시합니다.
+      alert('게시물 업로드에 실패했습니다. 다시 시도해 주세요.');
+      // 실패 시 에러 메시지를 표시합니다.
     });
   });
   
